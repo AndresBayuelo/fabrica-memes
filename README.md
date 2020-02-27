015ebb729b86403f7a420e0234e9c80e812b364b
Andres Bayuelo <andres.bayuelo@hotmail.com>
2020-02-26 23:00:56 -0500

    Configurando el proyecto

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Configurando plataforma "firebase"
+<ol>
+<li>Creando proyecto en "console.firebase.google.com"</li>
+<li>Crear base de datos de modo prueba.</li>
+<li>Crear bocket en storage eliminando la codificación de autenticación.</li>
+<li>Crear carpeta llamada "images".</li>
+<li>Crear aplicación web y generar el objeto de autenticación.</li>
+</ol>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+import { firestorePlugin } from 'vuefire'
+import router from './routes/index'
 
 Vue.config.productionTip = false
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBiI_sqGqe1AIWavF9Xu7ZLIMtk621pDnI",
+  authDomain: "fabrica-memes-92f97.firebaseapp.com",
+  databaseURL: "https://fabrica-memes-92f97.firebaseio.com",
+  projectId: "fabrica-memes-92f97",
+  storageBucket: "fabrica-memes-92f97.appspot.com",
+  messagingSenderId: "359962053327",
+  appId: "1:359962053327:web:b8e76904c67f1bbd2885ba",
+  measurementId: "G-38E3GXRVFZ"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const firestore = firebaseApp.firestore()
+export const storage = firebase.storage().ref('/images')
+
+Vue.use(firestorePlugin)
+
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  router,
+  components: {App}
 }).$mount('#app')
